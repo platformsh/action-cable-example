4652011c245ea5553628fb06c3595ed9ffc66781
Michael Skrynski <michael.skrynski@dkd.de>
2017-02-14 10:27:16 +0100

    use the platform.sh ENV to obtain relationships

--- config/initializers/platform_sh.rb
+++ config/initializers/platform_sh.rb
@@ -1,8 +1,11 @@
-require 'platform_sh'
+require 'base64'
+require 'json'
+relationships = JSON.parse(Base64.decode64(ENV['PLATFORM_RELATIONSHIPS']))
 
+return unless Rails.env.production?
 @config = PlatformSH::config
-redis_host = @config.fetch('relationships').fetch('cache').first.fetch('host')
-redis_port = @config.fetch('relationships').fetch('cache').first.fetch('port')
+redis_host = relationships.fetch('cache').first.fetch('host')
+redis_port = relationships.fetch('cache').first.fetch('port')
 
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{redis_host}:#{redis_port}/2" }
