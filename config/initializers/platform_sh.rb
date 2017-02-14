e4d00bc71c5e3861ecdb1aa06e7368ff173bde4d
Michael Skrynski <michael.skrynski@dkd.de>
2017-02-14 10:43:11 +0100

    resque in build phase

--- config/initializers/platform_sh.rb
+++ config/initializers/platform_sh.rb
@@ -1,18 +1,21 @@
 require 'base64'
 require 'json'
 
-relationships = JSON.parse(Base64.decode64(ENV['PLATFORM_RELATIONSHIPS']))
-redis_host = relationships.fetch('cache').first.fetch('host')
-redis_port = relationships.fetch('cache').first.fetch('port')
+begin
+  relationships = JSON.parse(Base64.decode64(ENV['PLATFORM_RELATIONSHIPS']))
+  redis_host = relationships.fetch('cache').first.fetch('host')
+  redis_port = relationships.fetch('cache').first.fetch('port')
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_host}:#{redis_port}/2" }
-end
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://#{redis_host}:#{redis_port}/2" }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_host}:#{redis_port}/2" }
-end
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://#{redis_host}:#{redis_port}/2" }
+  end
 
-Rails.application.configure do |config|
-  config.action_cable.url = "redis://#{redis_host}:#{redis_port}/1"
+  Rails.application.configure do |config|
+    config.action_cable.url = "redis://#{redis_host}:#{redis_port}/1"
+  end
+rescue
 end
