b750071735ced3f577470950e83d5e256d94f5ac
Michael Skrynski <michael.skrynski@dkd.de>
2017-02-14 13:14:54 +0100

    demonizing processes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_cable.disable_request_forgery_protection = true
-  # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
-  config.action_cable.url = 'http://master-7rqtwti-5xbxmker3tqas.eu.platform.sh/cable'
-  # config.action_cable.url = ENV['REDIS_URL']
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.url = ENV['REDIS_URL']
 end
